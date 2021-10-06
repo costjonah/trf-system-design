b5aeb68ac81021ee7674275a320b3aa379a71a90
Shavkat Shavkiev <shavkatshavkiev2002@gmail.com>
2021-10-06 11:27:58 -0400

    Added Jest tests and Stress test

--- reviews_server/models/index.js
+++ reviews_server/models/index.js
@@ -20,37 +20,6 @@ module.exports = {
         callBack(null, data)
       }
     })
-
-    // let query = `SELECT id AS review_id, rating,summary,recommend,response,body, date,reviewer_name,helpfulness FROM reviews WHERE product_id = ? and reported = 0 ORDER BY ${sort} DESC LIMIT ${count}`
-    // let queryArgs = [product_id, count]
-    // db.query(query,queryArgs, (err, data)=>{
-    //   if(err){
-    //     callBack(err)
-    //   } else {
-    //     Promise.all(
-    //       data.map(review => {
-    //         let newDate = new Date(review.date)
-    //         newDate.toISOString()
-    //         review.date = newDate
-
-    //         return new Promise((resolve, reject) => {
-    //           let queryString = `SELECT id, url FROM review_photos WHERE review_id = ${review.review_id}`
-    //           db.query(queryString, (err, photos) => {
-    //             if(err){
-    //               reject(err)
-    //             } else {
-    //               review.photos = photos
-    //               resolve(review)
-    //             }
-    //           })
-    //         })
-    //       })
-    //     )
-    //     .then(updatedData => {
-    //       callBack(null, data)
-    //     })
-    //   }
-    // })
   },
 
   getMeta: function(productID, callBack){
@@ -101,7 +70,6 @@ module.exports = {
         }
       })
 
-      // callBack(null, {product_id: productID,rating, recommend})
     })
   },
 
